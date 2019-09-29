3717cdf2539d1708ac2b9b72bd7158e81f9dbc80
Craig Douglas French <craigdfrench@users.noreply.github.com>
2019-09-28 21:50:47 -0400

    Add installer

---
+++ cmd/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+mkdir -p $GOPATH/{src,pkg,bin}
+go get -u github.com/golang/protobuf/protoc-gen-go
+go get -u github.com/craigdfrench/event
+rm /tmp/*.pid
+$GOPATH/src/github.com/craigdfrench/event/cmd/event-service/event-service.sh start
